cd3e01fc50338e97d7f4317fefbc58ad195a2c0a
hanjaek <hjk2512@naver.com>
2025-04-01 13:42:41 +0900

    Update imageClassifier code

--- lecture/lecture_02/image_classifier.py
+++ lecture/lecture_02/image_classifier.py
@@ -34,6 +34,28 @@ def run_classifier():
         plt.xlabel(class_names[train_labels[i]])
     plt.show()
 
+    ##### classifier train and predict - begin
+    epochs = 10 # 1500
+
+    mlp_classifier = ImageClassifier_MLP(img_shape_x=28, img_shape_y=28, num_labels=10)
+    mlp_classifier.build_model()
+    train_labels = ImageClassifier_MLP.to_onehotvec_label(train_labels, 10)
+    mlp_classifier.fit(train_imgs=train_images, train_labels=train_labels, num_epochs=epochs)
+
+    predicted_labels = mlp_classifier.predict(test_imgs=test_images)
+    predicted_labels = tf.math.argmax(input=predicted_labels, axis=1)
+
+    plt.figure(figsize=(10,10))
+    for i in range(25):
+        plt.subplot(5, 5, i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(test_images[i], cmap=plt.cm.binary)
+        plt.xlabel(class_names[predicted_labels[i]])
+    plt.show()
+    ##### classifier train and predict - end
+
 if __name__ == "__main__":
     # execute only if run as a script
     run_classifier()
